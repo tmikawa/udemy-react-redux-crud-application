099b65c324877c46db29b5486e315bfe30bd7cbb
tmikawa <tmikawa@ipsys.co.jp>
2020-04-26 14:10:52 +0900

    jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 function App() {
-  return (
-    <div><h1></h1>Hello World!!</div>
-  );
+ return (
+   <div><h1>Hello World!</h1></div>
+ );
 }
+
+// class App extends Component {
+//   render() {
+//     return <div>Hello, world!!!</div>;
+//   }
+// }
+//class App extends Component {
+//  render (){
+//    return React.createElement(
+//      "div",
+//      null,
+//      "Hello, world!!"
+//    );
+//  }
+//}
 
 export default App;
